c1046afd1d4908fa80ce8ab0af07ea1459af6e77
cassicakes <aguiar.cas@gmail.com>
2016-02-17 13:56:31 -0800

    user table and tested

---
+++ dbTest.js
@@ -0,0 +1,16 @@
+var db = require('./models');
+
+//create user
+db.user.create({
+	name: 'Brian',
+	email: 'bh@ga.co',
+	password: 'fishtacos'
+}).then(function(user){
+	console.log(user.get());
+	//find user
+	db.user.findById(1).then(function(user) {
+	console.log(user.get());
+	});
+});
+
+
