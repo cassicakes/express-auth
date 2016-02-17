bab6379cc7112593af2241b88dfec3c1e676e416
cassicakes <aguiar.cas@gmail.com>
2016-02-17 14:10:13 -0800

    more stuffs

--- dbTest.js
+++ dbTest.js
@@ -1,16 +1,19 @@
-var db = require('./models');
+// var db = require('./models');
 
-//create user
-db.user.create({
-	name: 'Brian',
-	email: 'bh@ga.co',
-	password: 'fishtacos'
-}).then(function(user){
-	console.log(user.get());
-	//find user
-	db.user.findById(1).then(function(user) {
-	console.log(user.get());
-	});
-});
+// //this page was just to initally test that my database is working
+
+
+// //create user
+// db.user.create({
+// 	name: 'Brian',
+// 	email: 'bh@ga.co',
+// 	password: 'fishtacos'
+// }).then(function(user){
+// 	console.log(user.get());
+// 	//find user
+// 	db.user.findById(1).then(function(user) {
+// 	console.log(user.get());
+// 	});
+// });
 
 
